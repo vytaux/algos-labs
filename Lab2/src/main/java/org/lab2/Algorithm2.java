44488132b1c6bb60e3f1b796e2b976e3e92c623a
Nwai Thingyan <nwaithingyan485@gmail.com>
2024-04-30 22:31:34 -0500

    First commit

--- Lab2/src/main/java/org/lab2/Algorithm2.java
+++ Lab2/src/main/java/org/lab2/Algorithm2.java
@@ -2,7 +2,34 @@ package org.lab2;
 
 public class Algorithm2 {
     public static int thirdLargest(int[] A) {
+        if (arr.length < 3) {
+            return null;
+        }
+
         int max = Integer.MIN_VALUE;
-        return 0;
+        int preMax = Integer.MIN_VALUE;
+        int prePreMax = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > max) {
+                prePreMax = preMax;
+                preMax = max;
+                max = num;
+            } else if (num > preMax && num < max) {
+                prePreMax = preMax;
+                preMax = num;
+            } else if (num > prePreMax && num < preMax) {
+                prePreMax = num;
+            } else if (num == max || num == preMax) {
+                if (num == max && preMax < num) {
+                    prePreMax = preMax;
+                    preMax = num;
+                } else if (num == preMax && prePreMax < num) {
+                    prePreMax = num;
+                }
+            }
+        }
+
+        return prePreMax;
     }
 }
